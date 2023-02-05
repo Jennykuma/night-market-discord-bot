639d9eebb1d8453d5d4986d08e2e7bf80bafab8d
Jenny Le <jennyle.dev@gmail.com>
2023-02-04 22:22:55 -0700

    Add scheduled messages via Cron

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,12 @@
-import { ActivityType, Client } from "discord.js";
+import { ActivityType, Client, TextChannel } from "discord.js";
 import ENV from "./Config";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
+import { shops, shopReminderEmbed } from "./commands/ShopReminder";
 
 const token = ENV.TOKEN;
+
+var CronJob = require('cron').CronJob;
 
 console.log("Bot is starting...");
 
@@ -11,11 +14,26 @@ const client = new Client({
   intents: []
 });
 
-client.on('ready', () => {
-  client.user?.setActivity('Night Market clowns', { type: ActivityType.Watching })
-});
+function scheduleShopReminderMessage() {
+  var shopReminderJob = new CronJob(
+    '*/30 * * * *',
+    function() {
+      client.channels.fetch('1071655921161351219')
+        .then(channel=>(channel as TextChannel).send({embeds: [shopReminderEmbed]}));
+    },
+    null,
+    true,
+    'America/Los_Angeles'
+  );
+  console.log('ShopReminderJob has initialized');
+}
 
 ready(client);
 interactionCreate(client);
+
+client.on('ready', (client) => {
+  client.user?.setActivity('Night Market clowns', { type: ActivityType.Watching });
+  scheduleShopReminderMessage();
+});
 
 client.login(token);
--- src/commands/ShopReminder.ts
+++ src/commands/ShopReminder.ts
@@ -1,25 +1,25 @@
 import { CommandInteraction, Client, EmbedBuilder } from "discord.js";
 import { Command } from "../Command";
 
+export const shops = ['Guild Shop', 'Pirate Ship Shop', 'PvP Shop', 'Condom Exchange Shop', 'Event Exchange Shop (if there is an event)']
+
+export const shopReminderEmbed = new EmbedBuilder()
+  .setColor('#e2725b')
+  .setTitle('Shop Reminder ⚠️')
+  .setDescription(`
+    ❗❗ 🇮 🇲 🇵 🇴 🇷 🇹 🇦 🇳 🇹 ❗❗
+
+    Remember to buy out your: 
+    - ${shops.join("\n - ")}`
+  )
+  .setTimestamp()
+  .setThumbnail('https://i.imgur.com/rlfvJPs.png')
+  .setFooter({ text: 'by Jennykuma' });
+
 export const ShopReminder: Command = {
   name: "shopreminder",
   description: "Returns a reminder for guildies to buyout all of the shops",
   run: async (client: Client, interaction: CommandInteraction) => {
-    
-    var shops = ['Guild Shop', 'Pirate Ship Shop', 'PvP Shop', 'Condom Exchange Shop', 'Event Exchange (if there is an event)']
-
-    const shopReminderEmbed = new EmbedBuilder()
-      .setColor('#e2725b')
-      .setTitle('Shop Reminder ⚠️')
-      .setDescription(`
-        ❗❗ 🇮 🇲 🇵 🇴 🇷 🇹 🇦 🇳 🇹 ❗❗
-
-        Remember to buy out your: 
-        - ${shops.join("\n - ")}`
-      )
-      .setTimestamp()
-      .setThumbnail('https://i.imgur.com/rlfvJPs.png')
-      .setFooter({ text: 'by Jennykuma' });
 
     interaction.followUp({
       ephemeral: true,
